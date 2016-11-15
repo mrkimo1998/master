43ac35e5917e1d3720fe9d917b3f035b812dfa7e
mrkimo1998 <i.kossmann98@gmail.com>
2016-11-15 18:17:37 +0100

    201611151817

---
+++ coding/newtab/index.php
@@ -0,0 +1,36 @@
+
+<head>
+	<title>Neuer Tab</title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href="main.css" />
+	<link href='https://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css' />
+</head>
+<html>
+	<body>
+		<div id=container>
+			<div id=banner>
+			<a href="#"><pre>
+xx  x   xxxx   xx     xx    xxxxx 
+xx x     xx    xxx   xxx   xx   xx
+xxx      xx    xx x x xx   xx   xx
+xx x     xx    xx  x  xx   xx   xx
+xx  x   xxxx   xx     xx    xxxxx
+			</pre></a>
+			</div>
+			<div id=navbar>
+				<ul>
+					<li><a href="https://duckduckgo.com/">Duck it!</a></li>
+					<li><a href="https://twitter.com/">Twitter</a></li>
+					<li><a href="https://github.com/mrkimo1998/master">GitHub</a></li>
+					<li><a href="#">#</a></li>
+					<li><a href="#">#</a></li>
+					<li><a href="#">#</a></li>
+				</ul>
+			</div>
+			<div id=dividor></div>
+			<div id=content>
+				<!-- Add Weather, Searchbar, News, etc here! -->
+			</div>
+		</div>
+	</body>
+</html>
