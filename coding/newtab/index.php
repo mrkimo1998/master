cc82fd642f7e798b8b00bf7b725d8800643d9a5a
mrkimo1998 <i.kossmann98@gmail.com>
2016-11-16 12:20:45 +0100

    201611161220

--- coding/newtab/index.php
+++ coding/newtab/index.php
@@ -1,36 +1,41 @@
 
-<head>
-	<title>Neuer Tab</title>
-	<meta charset="UTF-8" />
-	<link rel="stylesheet" type="text/css" href="main.css" />
-	<link href='https://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css' />
-</head>
-<html>
-	<body>
-		<div id=container>
-			<div id=banner>
-			<a href="#"><pre>
-xx  x   xxxx   xx     xx    xxxxx 
-xx x     xx    xxx   xxx   xx   xx
-xxx      xx    xx x x xx   xx   xx
-xx x     xx    xx  x  xx   xx   xx
-xx  x   xxxx   xx     xx    xxxxx
-			</pre></a>
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap-Basis-Vorlage</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="./addition.css" type="text/css">
+  </head>
+  <body style="background-color:#333">
+  	<div class="container">
+		<div class="row">
+			<!-- Make btns smaller ?-->
+  			<div class="col-md-4">
+			  <a href="https://twitter.com/" role="button"><img src="img/twitter.svg" alt="" class="img-responsive img-rounded" style="background-color:#FFF"></a>
 			</div>
-			<div id=navbar>
-				<ul>
-					<li><a href="https://duckduckgo.com/">Duck it!</a></li>
-					<li><a href="https://twitter.com/">Twitter</a></li>
-					<li><a href="https://github.com/mrkimo1998/master">GitHub</a></li>
-					<li><a href="#">#</a></li>
-					<li><a href="#">#</a></li>
-					<li><a href="#">#</a></li>
-				</ul>
+  			<div class="col-md-4">
+			  <a href="https://github.com/mrkimo1998/master" role="button"><img src="img/github.svg" alt="" class="img-responsive img-rounded" style="background-color:#FFF"></a>
 			</div>
-			<div id=dividor></div>
-			<div id=content>
-				<!-- Add Weather, Searchbar, News, etc here! -->
+  			<div class="col-md-4">
+			  <a href="https://pinterest.com" role="button"><img src="img/pinterest.svg" alt="" class="img-responsive img-rounded" style="background-color:#FFF"></a>
 			</div>
 		</div>
-	</body>
+		<div class="row">
+   			<div class="col-lg-12">
+				<!-- Embed search here! +  weather etc-->
+			</div>
+  		</div>
+	</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  	<footer class="footer">
+		<div>
+			<p>Icons made by <a href="http://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+			</p>
+		</div>
+  	</footer>
+  </body>
 </html>
